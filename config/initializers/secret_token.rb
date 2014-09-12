50906534f53f7e17d648ca494f49f7db7a7ef05c
Nanthakumar R <nanthakumarr@sourcebitss-iMac-3.local>
2014-09-12 17:21:51 +0530

    foundationDB gem added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItAssetManagement::Application.config.secret_key_base = 'cdc39367e91cc9883d7c6c95166897a27963120bf91c5329137b64915113a50e22245fe3d70bef89ff29ebe5eb6477740d35d665f33da14bf257993a107166fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItAssetManagement::Application.config.session_store :cookie_store, key: '_it-asset-management_session'
